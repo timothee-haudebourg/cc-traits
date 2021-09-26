fb4c9c55a14a05022ed4024c3b8ce2e3523d0407
Timothée Haudebourg <timothee.haudebourg@ens-rennes.fr>
2021-09-26 23:48:00 +0200

    Fix slab and smallvec features.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [0.5.0] - 2021-09-22
+## [0.5.1] - 2021-09-26
+- Fix features `slab` and `smallvec`.
+
+## [0.5.0] - 2021-09-22 [YANKED]
+### Yanking reason
+- Errors with features `slab` and `smallvec`.
+
 ### Added 
 - `Keyed` and `KeyedRef` traits.
--- src/impls/smallvec.rs
+++ src/impls/smallvec.rs
@@ -1,6 +1,6 @@
 use crate::{
-	Back, BackMut, Capacity, Clear, Collection, CollectionMut, CollectionRef, Front, FrontMut, Len,
-	PopBack, PushBack, Remove, Reserve, WithCapacity,
+	Back, BackMut, Capacity, Clear, Collection, CollectionMut, CollectionRef, Front, FrontMut,
+	Iter, IterMut, Len, PopBack, PushBack, Remove, Reserve, WithCapacity,
 };
 use smallvec::{Array, SmallVec};
 
@@ -42,59 +42,65 @@ impl<A: Array> Len for SmallVec<A> {
 }
 
 impl<A: Array> Capacity for SmallVec<A> {
+	#[inline(always)]
 	fn capacity(&self) -> usize {
 		self.capacity()
 	}
 }
 
 impl<A: Array> Reserve for SmallVec<A> {
+	#[inline(always)]
 	fn reserve(&mut self, additional: usize) {
 		self.reserve(additional)
 	}
 }
 
 impl<A: Array> Back for SmallVec<A> {
+	#[inline(always)]
 	fn back(&self) -> Option<&A::Item> {
 		self.last()
 	}
 }
 
 impl<A: Array> BackMut for SmallVec<A> {
+	#[inline(always)]
 	fn back_mut(&mut self) -> Option<&mut A::Item> {
 		self.last_mut()
 	}
 }
 
 impl<A: Array> Front for SmallVec<A> {
+	#[inline(always)]
 	fn front(&self) -> Option<&A::Item> {
 		self.first()
 	}
 }
 
 impl<A: Array> FrontMut for SmallVec<A> {
+	#[inline(always)]
 	fn front_mut(&mut self) -> Option<&mut A::Item> {
 		self.first_mut()
 	}
 }
 
 impl<A: Array> PushBack for SmallVec<A> {
-	type Output<'a>
-	where
-		Self: 'a,
-	= ();
+	type Output = ();
 
+	#[inline(always)]
 	fn push_back(&mut self, t: A::Item) {
 		self.push(t)
 	}
 }
 
 impl<A: Array> PopBack for SmallVec<A> {
+	#[inline(always)]
 	fn pop_back(&mut self) -> Option<A::Item> {
 		self.pop()
 	}
 }
 
 impl<A: Array> Remove<usize> for SmallVec<A> {
+	#[inline(always)]
 	fn remove(&mut self, index: usize) -> Option<A::Item> {
 		if index < self.len() {
 			Some(self.remove(index))
@@ -105,7 +111,32 @@ impl<A: Array> Remove<usize> for SmallVec<A> {
 }
 
 impl<A: Array> Clear for SmallVec<A> {
+	#[inline(always)]
 	fn clear(&mut self) {
 		self.clear()
+	}
+}
+
+impl<A: Array> Iter for SmallVec<A> {
+	type Iter<'a>
+	where
+		A: 'a,
+	= std::slice::Iter<'a, A::Item>;
+
+	#[inline(always)]
+	fn iter(&self) -> Self::Iter<'_> {
+		self.as_slice().iter()
+	}
+}
+
+impl<A: Array> IterMut for SmallVec<A> {
+	type IterMut<'a>
+	where
+		A: 'a,
+	= std::slice::IterMut<'a, A::Item>;
+
+	#[inline(always)]
+	fn iter_mut(&mut self) -> Self::IterMut<'_> {
+		self.as_mut_slice().iter_mut()
 	}
 }
