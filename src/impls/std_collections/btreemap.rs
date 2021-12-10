efbcc563a680edc246c974726c1a6d24abf36af0
David Ewert <dewert10@gmail.com>
2021-12-09 20:40:02 -0800

    used aliases to make copy-pasting between impl mods easier

--- src/impls/std_collections/btreemap.rs
+++ src/impls/std_collections/btreemap.rs
@@ -1,14 +1,14 @@
 use crate::{Clear, Collection, CollectionMut, CollectionRef, Entry, EntryApi, Get, GetKeyValue, GetMut, Iter, Keyed, KeyedRef, KeyVacantEntry, Len, MapInsert, MapIter, MapIterMut, OccupiedEntry, Remove, VacantEntry};
 use std::{
 	borrow::Borrow,
-	collections::{btree_map, BTreeMap},
+	collections::{btree_map as this_mod, BTreeMap as ThisMap},
 };
 
-impl<K, V> Collection for BTreeMap<K, V> {
+impl<K, V> Collection for ThisMap<K, V> {
 	type Item = V;
 }
 
-impl<K, V> CollectionRef for BTreeMap<K, V> {
+impl<K, V> CollectionRef for ThisMap<K, V> {
 	type ItemRef<'a>
 	where
 		Self: 'a,
@@ -17,7 +17,7 @@ impl<K, V> CollectionRef for BTreeMap<K, V> {
 	crate::covariant_item_ref!();
 }
 
-impl<K, V> CollectionMut for BTreeMap<K, V> {
+impl<K, V> CollectionMut for ThisMap<K, V> {
 	type ItemMut<'a>
 	where
 		Self: 'a,
@@ -26,11 +26,11 @@ impl<K, V> CollectionMut for BTreeMap<K, V> {
 	crate::covariant_item_mut!();
 }
 
-impl<K, V> Keyed for BTreeMap<K, V> {
+impl<K, V> Keyed for ThisMap<K, V> {
 	type Key = K;
 }
 
-impl<K, V> KeyedRef for BTreeMap<K, V> {
+impl<K, V> KeyedRef for ThisMap<K, V> {
 	type KeyRef<'a>
 	where
 		Self: 'a,
@@ -39,7 +39,7 @@ impl<K, V> KeyedRef for BTreeMap<K, V> {
 	crate::covariant_key_ref!();
 }
 
-impl<K, V> Len for BTreeMap<K, V> {
+impl<K, V> Len for ThisMap<K, V> {
 	#[inline(always)]
 	fn len(&self) -> usize {
 		self.len()
@@ -51,7 +51,7 @@ impl<K, V> Len for BTreeMap<K, V> {
 	}
 }
 
-impl<'a, Q, K: Ord, V> Get<&'a Q> for BTreeMap<K, V>
+impl<'a, Q, K: Ord, V> Get<&'a Q> for ThisMap<K, V>
 where
 	K: Borrow<Q>,
 	Q: Ord + ?Sized,
@@ -62,7 +62,7 @@ where
 	}
 }
 
-impl<'a, Q, K: Ord, V> GetKeyValue<&'a Q> for BTreeMap<K, V>
+impl<'a, Q, K: Ord, V> GetKeyValue<&'a Q> for ThisMap<K, V>
 where
 	K: Borrow<Q>,
 	Q: Ord + ?Sized,
@@ -73,7 +73,7 @@ where
 	}
 }
 
-impl<'a, Q, K: Ord, V> GetMut<&'a Q> for BTreeMap<K, V>
+impl<'a, Q, K: Ord, V> GetMut<&'a Q> for ThisMap<K, V>
 where
 	K: Borrow<Q>,
 	Q: Ord + ?Sized,
@@ -84,7 +84,7 @@ where
 	}
 }
 
-impl<K: Ord, V> MapInsert<K> for BTreeMap<K, V> {
+impl<K: Ord, V> MapInsert<K> for ThisMap<K, V> {
 	type Output = Option<V>;
 
 	#[inline(always)]
@@ -93,7 +93,7 @@ impl<K: Ord, V> MapInsert<K> for BTreeMap<K, V> {
 	}
 }
 
-impl<'a, Q, K: Ord, V> Remove<&'a Q> for BTreeMap<K, V>
+impl<'a, Q, K: Ord, V> Remove<&'a Q> for ThisMap<K, V>
 where
 	K: Borrow<Q>,
 	Q: Ord + ?Sized,
@@ -104,18 +104,18 @@ where
 	}
 }
 
-impl<K: Ord, V> Clear for BTreeMap<K, V> {
+impl<K: Ord, V> Clear for ThisMap<K, V> {
 	#[inline(always)]
 	fn clear(&mut self) {
 		self.clear()
 	}
 }
 
-impl<K, V> Iter for BTreeMap<K, V> {
+impl<K, V> Iter for ThisMap<K, V> {
 	type Iter<'a>
 	where
 		Self: 'a,
-	= std::collections::btree_map::Values<'a, K, V>;
+	= this_mod::Values<'a, K, V>;
 
 	#[inline(always)]
 	fn iter(&self) -> Self::Iter<'_> {
@@ -123,11 +123,11 @@ impl<K, V> Iter for BTreeMap<K, V> {
 	}
 }
 
-impl<K, V> MapIter for BTreeMap<K, V> {
+impl<K, V> MapIter for ThisMap<K, V> {
 	type Iter<'a>
 	where
 		Self: 'a,
-	= std::collections::btree_map::Iter<'a, K, V>;
+	= this_mod::Iter<'a, K, V>;
 
 	#[inline(always)]
 	fn iter(&self) -> Self::Iter<'_> {
@@ -135,11 +135,11 @@ impl<K, V> MapIter for BTreeMap<K, V> {
 	}
 }
 
-impl<K, V> MapIterMut for BTreeMap<K, V> {
+impl<K, V> MapIterMut for ThisMap<K, V> {
 	type IterMut<'a>
 	where
 		Self: 'a,
-	= std::collections::btree_map::IterMut<'a, K, V>;
+	= this_mod::IterMut<'a, K, V>;
 
 	#[inline(always)]
 	fn iter_mut(&mut self) -> Self::IterMut<'_> {
@@ -147,150 +147,150 @@ impl<K, V> MapIterMut for BTreeMap<K, V> {
 	}
 }
 
-impl<'a, K: Ord, V> OccupiedEntry<'a> for btree_map::OccupiedEntry<'a, K, V> {
+impl<'a, K: Ord, V> OccupiedEntry<'a> for this_mod::OccupiedEntry<'a, K, V> {
 	type K = K;
 	type V = V;
 
 	#[inline(always)]
 	fn key(&self) -> &Self::K {
-		btree_map::OccupiedEntry::key(self)
+		this_mod::OccupiedEntry::key(self)
 	}
 
 	#[inline(always)]
 	fn remove_entry(self) -> (Self::K, Self::V) {
-		btree_map::OccupiedEntry::remove_entry(self)
+		this_mod::OccupiedEntry::remove_entry(self)
 	}
 
 	#[inline(always)]
 	fn get(&self) -> &Self::V {
-		btree_map::OccupiedEntry::get(self)
+		this_mod::OccupiedEntry::get(self)
 	}
 
 	#[inline(always)]
 	fn get_mut(&mut self) -> &mut Self::V {
-		btree_map::OccupiedEntry::get_mut(self)
+		this_mod::OccupiedEntry::get_mut(self)
 	}
 
 	#[inline(always)]
 	fn into_mut(self) -> &'a mut Self::V {
-		btree_map::OccupiedEntry::into_mut(self)
+		this_mod::OccupiedEntry::into_mut(self)
 	}
 
 	#[inline(always)]
 	fn insert(&mut self, value: Self::V) -> Self::V {
-		btree_map::OccupiedEntry::insert(self, value)
+		this_mod::OccupiedEntry::insert(self, value)
 	}
 
 	#[inline(always)]
 	fn remove(self) -> Self::V {
-		btree_map::OccupiedEntry::remove(self)
+		this_mod::OccupiedEntry::remove(self)
 	}
 }
 
-impl<'a, K: Ord, V> VacantEntry<'a> for btree_map::VacantEntry<'a, K, V> {
+impl<'a, K: Ord, V> VacantEntry<'a> for this_mod::VacantEntry<'a, K, V> {
 	type K = K;
 	type V = V;
 
 	#[inline(always)]
 	fn into_key(self) -> Self::K {
-		btree_map::VacantEntry::into_key(self)
+		this_mod::VacantEntry::into_key(self)
 	}
 
 	#[inline(always)]
 	fn insert(self, value: Self::V) -> &'a mut Self::V {
-		btree_map::VacantEntry::insert(self, value)
+		this_mod::VacantEntry::insert(self, value)
 	}
 }
 
-impl<'a, K: Ord, V> KeyVacantEntry<'a> for btree_map::VacantEntry<'a, K, V> {
+impl<'a, K: Ord, V> KeyVacantEntry<'a> for this_mod::VacantEntry<'a, K, V> {
 	#[inline(always)]
 	fn key(&self) -> &Self::K {
-		btree_map::VacantEntry::key(self)
+		this_mod::VacantEntry::key(self)
 	}
 }
 
-impl<K: Ord, V> EntryApi for BTreeMap<K, V> {
+impl<K: Ord, V> EntryApi for ThisMap<K, V> {
 	type Occ<'a>
 	where
 		Self: 'a,
-	= btree_map::OccupiedEntry<'a, K, V>;
+	= this_mod::OccupiedEntry<'a, K, V>;
 	type Vac<'a>
 	where
 		Self: 'a,
-	= btree_map::VacantEntry<'a, K, V>;
+	= this_mod::VacantEntry<'a, K, V>;
 
 	#[inline(always)]
 	fn entry(&mut self, key: Self::Key) -> Entry<Self::Occ<'_>, Self::Vac<'_>> {
-		match BTreeMap::entry(self, key) {
-			btree_map::Entry::Occupied(o) => Entry::Occupied(o),
-			btree_map::Entry::Vacant(v) => Entry::Vacant(v),
+		match ThisMap::entry(self, key) {
+			this_mod::Entry::Occupied(o) => Entry::Occupied(o),
+			this_mod::Entry::Vacant(v) => Entry::Vacant(v),
 		}
 	}
 }
 
 #[cfg(feature="raw-api")]
-impl<'a, K, V> OccupiedEntry<'a> for btree_map::RawOccupiedEntryMut<'a, K, V> {
+impl<'a, K, V> OccupiedEntry<'a> for this_mod::RawOccupiedEntryMut<'a, K, V> {
 	type K = K;
 	type V = V;
 
 	#[inline(always)]
 	fn key(&self) -> &Self::K {
-		btree_map::RawOccupiedEntryMut::key(self)
+		this_mod::RawOccupiedEntryMut::key(self)
 	}
 
 	#[inline(always)]
 	fn remove_entry(self) -> (Self::K, Self::V) {
-		btree_map::RawOccupiedEntryMut::remove_entry(self)
+		this_mod::RawOccupiedEntryMut::remove_entry(self)
 	}
 
 	#[inline(always)]
 	fn get(&self) -> &Self::V {
-		btree_map::RawOccupiedEntryMut::get(self)
+		this_mod::RawOccupiedEntryMut::get(self)
 	}
 
 	#[inline(always)]
 	fn get_mut(&mut self) -> &mut Self::V {
-		btree_map::RawOccupiedEntryMut::get_mut(self)
+		this_mod::RawOccupiedEntryMut::get_mut(self)
 	}
 
 	#[inline(always)]
 	fn into_mut(self) -> &'a mut Self::V {
-		btree_map::RawOccupiedEntryMut::into_mut(self)
+		this_mod::RawOccupiedEntryMut::into_mut(self)
 	}
 
 	#[inline(always)]
 	fn insert(&mut self, value: Self::V) -> Self::V {
-		btree_map::RawOccupiedEntryMut::insert(self, value)
+		this_mod::RawOccupiedEntryMut::insert(self, value)
 	}
 
 	#[inline(always)]
 	fn remove(self) -> Self::V {
-		btree_map::RawOccupiedEntryMut::remove(self)
+		this_mod::RawOccupiedEntryMut::remove(self)
 	}
 }
 
 
 #[cfg(feature="raw-api")]
-impl<'a, K, V> crate::RawVacantEntry<'a> for btree_map::RawOccupiedEntryMut<'a, K, V> {
+impl<'a, K, V> crate::RawVacantEntry<'a> for this_mod::RawOccupiedEntryMut<'a, K, V> {
 	type K = K;
 	type V = V;
 
 	fn insert(self, key: Self::K, value: Self::V) -> (&'a mut Self::K, &'a mut Self::V) {
-		btree_map::RawOccupiedEntryMut::insert(self, key, value)
+		this_mod::RawOccupiedEntryMut::insert(self, key, value)
 	}
 }
 
 
 #[cfg(feature="raw-api")]
-impl<Q: Ord + ToOwned<Owned = K>, K: Ord, V> crate::EntryRefApi<Q> for BTreeMap<K, V>{
-	type Occ<'a> where Self: 'a = btree_map::RawOccupiedEntryMut<'a, K, V>;
-	type Vac<'a, 'b> where Self: 'a = crate::KeyedRawVacantEntry<'a, 'b, Q, btree_map::RawVacantEntryMut<'a, K, V>>;
+impl<Q: Ord + ToOwned<Owned = K>, K: Ord, V> crate::EntryRefApi<Q> for ThisMap<K, V>{
+	type Occ<'a> where Self: 'a = this_mod::RawOccupiedEntryMut<'a, K, V>;
+	type Vac<'a, 'b> where Self: 'a = crate::KeyedRawVacantEntry<'a, 'b, Q, this_mod::RawVacantEntryMut<'a, K, V>>;
 
 	fn entry_ref<'a, 'b: 'a>(&'a mut self, key: &'b Q) -> Entry<Self::Occ<'a>, Self::Vac<'a, 'b>> {
 		let mut raw = self.raw_entry_mut();
 		match raw.from_key(key) {
-			btree_map::RawEntryMut::Occupied(occ) => {Entry::Occupied(occ)}
-			btree_map::RawEntryMut::Vacant(vac) => {
+			this_mod::RawEntryMut::Occupied(occ) => {Entry::Occupied(occ)}
+			this_mod::RawEntryMut::Vacant(vac) => {
 				Entry::Vacant(crate::KeyedRawVacantEntry{key, raw: vac})
 			}
 		}
--- src/impls/std_collections/hashmap.rs
+++ src/impls/std_collections/hashmap.rs
@@ -1,15 +1,15 @@
 use crate::{Clear, Collection, CollectionMut, CollectionRef, Entry, EntryApi, Get, GetKeyValue, GetMut, Iter, Keyed, KeyedRef, KeyVacantEntry, Len, MapInsert, MapIter, MapIterMut, OccupiedEntry, Remove, VacantEntry};
 use std::{
 	borrow::Borrow,
-	collections::{hash_map, HashMap},
+	collections::{hash_map as this_mod, HashMap as ThisMap},
 	hash::{BuildHasher, Hash},
 };
 
-impl<K, V, S: BuildHasher> Collection for HashMap<K, V, S> {
+impl<K, V, S: BuildHasher> Collection for ThisMap<K, V, S> {
 	type Item = V;
 }
 
-impl<K, V, S: BuildHasher> CollectionRef for HashMap<K, V, S> {
+impl<K, V, S: BuildHasher> CollectionRef for ThisMap<K, V, S> {
 	type ItemRef<'a>
 	where
 		Self: 'a,
@@ -18,7 +18,7 @@ impl<K, V, S: BuildHasher> CollectionRef for HashMap<K, V, S> {
 	crate::covariant_item_ref!();
 }
 
-impl<K, V, S: BuildHasher> CollectionMut for HashMap<K, V, S> {
+impl<K, V, S: BuildHasher> CollectionMut for ThisMap<K, V, S> {
 	type ItemMut<'a>
 	where
 		Self: 'a,
@@ -27,11 +27,11 @@ impl<K, V, S: BuildHasher> CollectionMut for HashMap<K, V, S> {
 	crate::covariant_item_mut!();
 }
 
-impl<K, V, S: BuildHasher> Keyed for HashMap<K, V, S> {
+impl<K, V, S: BuildHasher> Keyed for ThisMap<K, V, S> {
 	type Key = K;
 }
 
-impl<K, V, S: BuildHasher> KeyedRef for HashMap<K, V, S> {
+impl<K, V, S: BuildHasher> KeyedRef for ThisMap<K, V, S> {
 	type KeyRef<'a>
 	where
 		Self: 'a,
@@ -40,7 +40,7 @@ impl<K, V, S: BuildHasher> KeyedRef for HashMap<K, V, S> {
 	crate::covariant_key_ref!();
 }
 
-impl<K, V, S: BuildHasher> Len for HashMap<K, V, S> {
+impl<K, V, S: BuildHasher> Len for ThisMap<K, V, S> {
 	#[inline(always)]
 	fn len(&self) -> usize {
 		self.len()
@@ -52,7 +52,7 @@ impl<K, V, S: BuildHasher> Len for HashMap<K, V, S> {
 	}
 }
 
-impl<'a, Q, K: Hash + Eq, V, S: BuildHasher> Get<&'a Q> for HashMap<K, V, S>
+impl<'a, Q, K: Hash + Eq, V, S: BuildHasher> Get<&'a Q> for ThisMap<K, V, S>
 where
 	K: Borrow<Q>,
 	Q: Hash + Eq + ?Sized,
@@ -63,7 +63,7 @@ where
 	}
 }
 
-impl<'a, Q, K: Hash + Eq, V, S: BuildHasher> GetMut<&'a Q> for HashMap<K, V, S>
+impl<'a, Q, K: Hash + Eq, V, S: BuildHasher> GetMut<&'a Q> for ThisMap<K, V, S>
 where
 	K: Borrow<Q>,
 	Q: Hash + Eq + ?Sized,
@@ -74,7 +74,7 @@ where
 	}
 }
 
-impl<'a, Q, K: Hash + Eq, V, S: BuildHasher> GetKeyValue<&'a Q> for HashMap<K, V, S>
+impl<'a, Q, K: Hash + Eq, V, S: BuildHasher> GetKeyValue<&'a Q> for ThisMap<K, V, S>
 where
 	K: Borrow<Q>,
 	Q: Hash + Eq + ?Sized,
@@ -85,7 +85,7 @@ where
 	}
 }
 
-impl<K: Hash + Eq, V, S: BuildHasher> MapInsert<K> for HashMap<K, V, S> {
+impl<K: Hash + Eq, V, S: BuildHasher> MapInsert<K> for ThisMap<K, V, S> {
 	type Output = Option<V>;
 
 	#[inline(always)]
@@ -94,7 +94,7 @@ impl<K: Hash + Eq, V, S: BuildHasher> MapInsert<K> for HashMap<K, V, S> {
 	}
 }
 
-impl<'a, Q, K: Hash + Eq, V, S: BuildHasher> Remove<&'a Q> for HashMap<K, V, S>
+impl<'a, Q, K: Hash + Eq, V, S: BuildHasher> Remove<&'a Q> for ThisMap<K, V, S>
 where
 	K: Borrow<Q>,
 	Q: Hash + Eq + ?Sized,
@@ -105,18 +105,18 @@ where
 	}
 }
 
-impl<K, V, S: BuildHasher> Clear for HashMap<K, V, S> {
+impl<K, V, S: BuildHasher> Clear for ThisMap<K, V, S> {
 	#[inline(always)]
 	fn clear(&mut self) {
 		self.clear()
 	}
 }
 
-impl<K, V, S: BuildHasher> Iter for HashMap<K, V, S> {
+impl<K, V, S: BuildHasher> Iter for ThisMap<K, V, S> {
 	type Iter<'a>
 	where
 		Self: 'a,
-	= std::collections::hash_map::Values<'a, K, V>;
+	= this_mod::Values<'a, K, V>;
 
 	#[inline(always)]
 	fn iter(&self) -> Self::Iter<'_> {
@@ -124,11 +124,11 @@ impl<K, V, S: BuildHasher> Iter for HashMap<K, V, S> {
 	}
 }
 
-impl<K, V, S: BuildHasher> MapIter for HashMap<K, V, S> {
+impl<K, V, S: BuildHasher> MapIter for ThisMap<K, V, S> {
 	type Iter<'a>
 	where
 		Self: 'a,
-	= std::collections::hash_map::Iter<'a, K, V>;
+	= this_mod::Iter<'a, K, V>;
 
 	#[inline(always)]
 	fn iter(&self) -> Self::Iter<'_> {
@@ -136,11 +136,11 @@ impl<K, V, S: BuildHasher> MapIter for HashMap<K, V, S> {
 	}
 }
 
-impl<K, V, S: BuildHasher> MapIterMut for HashMap<K, V, S> {
+impl<K, V, S: BuildHasher> MapIterMut for ThisMap<K, V, S> {
 	type IterMut<'a>
 	where
 		Self: 'a,
-	= std::collections::hash_map::IterMut<'a, K, V>;
+	= this_mod::IterMut<'a, K, V>;
 
 	#[inline(always)]
 	fn iter_mut(&mut self) -> Self::IterMut<'_> {
@@ -148,150 +148,150 @@ impl<K, V, S: BuildHasher> MapIterMut for HashMap<K, V, S> {
 	}
 }
 
-impl<'a, K, V> OccupiedEntry<'a> for hash_map::OccupiedEntry<'a, K, V> {
+impl<'a, K, V> OccupiedEntry<'a> for this_mod::OccupiedEntry<'a, K, V> {
 	type K = K;
 	type V = V;
 
 	#[inline(always)]
 	fn key(&self) -> &Self::K {
-		hash_map::OccupiedEntry::key(self)
+		this_mod::OccupiedEntry::key(self)
 	}
 
 	#[inline(always)]
 	fn remove_entry(self) -> (Self::K, Self::V) {
-		hash_map::OccupiedEntry::remove_entry(self)
+		this_mod::OccupiedEntry::remove_entry(self)
 	}
 
 	#[inline(always)]
 	fn get(&self) -> &Self::V {
-		hash_map::OccupiedEntry::get(self)
+		this_mod::OccupiedEntry::get(self)
 	}
 
 	#[inline(always)]
 	fn get_mut(&mut self) -> &mut Self::V {
-		hash_map::OccupiedEntry::get_mut(self)
+		this_mod::OccupiedEntry::get_mut(self)
 	}
 
 	#[inline(always)]
 	fn into_mut(self) -> &'a mut Self::V {
-		hash_map::OccupiedEntry::into_mut(self)
+		this_mod::OccupiedEntry::into_mut(self)
 	}
 
 	#[inline(always)]
 	fn insert(&mut self, value: Self::V) -> Self::V {
-		hash_map::OccupiedEntry::insert(self, value)
+		this_mod::OccupiedEntry::insert(self, value)
 	}
 
 	#[inline(always)]
 	fn remove(self) -> Self::V {
-		hash_map::OccupiedEntry::remove(self)
+		this_mod::OccupiedEntry::remove(self)
 	}
 }
 
-impl<'a, K, V> VacantEntry<'a> for hash_map::VacantEntry<'a, K, V> {
+impl<'a, K, V> VacantEntry<'a> for this_mod::VacantEntry<'a, K, V> {
 	type K = K;
 	type V = V;
 
 	#[inline(always)]
 	fn into_key(self) -> Self::K {
-		hash_map::VacantEntry::into_key(self)
+		this_mod::VacantEntry::into_key(self)
 	}
 
 	#[inline(always)]
 	fn insert(self, value: Self::V) -> &'a mut Self::V {
-		hash_map::VacantEntry::insert(self, value)
+		this_mod::VacantEntry::insert(self, value)
 	}
 }
 
-impl<'a, K, V> KeyVacantEntry<'a> for hash_map::VacantEntry<'a, K, V> {
+impl<'a, K, V> KeyVacantEntry<'a> for this_mod::VacantEntry<'a, K, V> {
 	#[inline(always)]
 	fn key(&self) -> &Self::K {
-		hash_map::VacantEntry::key(self)
+		this_mod::VacantEntry::key(self)
 	}
 }
 
-impl<K: Hash + Eq, V, S: BuildHasher> EntryApi for HashMap<K, V, S> {
+impl<K: Hash + Eq, V, S: BuildHasher> EntryApi for ThisMap<K, V, S> {
 	type Occ<'a>
 	where
 		Self: 'a,
-	= hash_map::OccupiedEntry<'a, K, V>;
+	= this_mod::OccupiedEntry<'a, K, V>;
 	type Vac<'a>
 	where
 		Self: 'a,
-	= hash_map::VacantEntry<'a, K, V>;
+	= this_mod::VacantEntry<'a, K, V>;
 
 	#[inline(always)]
 	fn entry(&mut self, key: Self::Key) -> Entry<Self::Occ<'_>, Self::Vac<'_>> {
-		match HashMap::entry(self, key) {
-			hash_map::Entry::Occupied(o) => Entry::Occupied(o),
-			hash_map::Entry::Vacant(v) => Entry::Vacant(v),
+		match ThisMap::entry(self, key) {
+			this_mod::Entry::Occupied(o) => Entry::Occupied(o),
+			this_mod::Entry::Vacant(v) => Entry::Vacant(v),
 		}
 	}
 }
 
 #[cfg(feature="raw-api")]
-impl<'a, K, V, S: BuildHasher> OccupiedEntry<'a> for hash_map::RawOccupiedEntryMut<'a, K, V, S> {
+impl<'a, K, V, S: BuildHasher> OccupiedEntry<'a> for this_mod::RawOccupiedEntryMut<'a, K, V, S> {
 	type K = K;
 	type V = V;
 
 	#[inline(always)]
 	fn key(&self) -> &Self::K {
-		hash_map::RawOccupiedEntryMut::key(self)
+		this_mod::RawOccupiedEntryMut::key(self)
 	}
 
 	#[inline(always)]
 	fn remove_entry(self) -> (Self::K, Self::V) {
-		hash_map::RawOccupiedEntryMut::remove_entry(self)
+		this_mod::RawOccupiedEntryMut::remove_entry(self)
 	}
 
 	#[inline(always)]
 	fn get(&self) -> &Self::V {
-		hash_map::RawOccupiedEntryMut::get(self)
+		this_mod::RawOccupiedEntryMut::get(self)
 	}
 
 	#[inline(always)]
 	fn get_mut(&mut self) -> &mut Self::V {
-		hash_map::RawOccupiedEntryMut::get_mut(self)
+		this_mod::RawOccupiedEntryMut::get_mut(self)
 	}
 
 	#[inline(always)]
 	fn into_mut(self) -> &'a mut Self::V {
-		hash_map::RawOccupiedEntryMut::into_mut(self)
+		this_mod::RawOccupiedEntryMut::into_mut(self)
 	}
 
 	#[inline(always)]
 	fn insert(&mut self, value: Self::V) -> Self::V {
-		hash_map::RawOccupiedEntryMut::insert(self, value)
+		this_mod::RawOccupiedEntryMut::insert(self, value)
 	}
 
 	#[inline(always)]
 	fn remove(self) -> Self::V {
-		hash_map::RawOccupiedEntryMut::remove(self)
+		this_mod::RawOccupiedEntryMut::remove(self)
 	}
 }
 
 
 #[cfg(feature="raw-api")]
-impl<'a, K, V, S: BuildHasher> crate::RawVacantEntry<'a> for hash_map::RawOccupiedEntryMut<'a, K, V, S> {
+impl<'a, K, V, S: BuildHasher> crate::RawVacantEntry<'a> for this_mod::RawOccupiedEntryMut<'a, K, V, S> {
 	type K = K;
 	type V = V;
 
 	fn insert(self, key: Self::K, value: Self::V) -> (&'a mut Self::K, &'a mut Self::V) {
-		hash_map::RawOccupiedEntryMut::insert(self, key, value)
+		this_mod::RawOccupiedEntryMut::insert(self, key, value)
 	}
 }
 
 
 #[cfg(feature="raw-api")]
-impl<Q: Hash + Eq + ToOwned<Owned = K>, K: Hash + Eq, V, S: BuildHasher> crate::EntryRefApi<Q> for HashMap<K, V, S>{
-	type Occ<'a> where Self: 'a = hash_map::RawOccupiedEntryMut<'a, K, V, S>;
-	type Vac<'a, 'b> where Self: 'a = crate::KeyedRawVacantEntry<'a, 'b, Q, hash_map::RawVacantEntryMut<'a, K, V, S>>;
+impl<Q: Hash + Eq + ToOwned<Owned = K>, K: Hash + Eq, V, S: BuildHasher> crate::EntryRefApi<Q> for ThisMap<K, V, S>{
+	type Occ<'a> where Self: 'a = this_mod::RawOccupiedEntryMut<'a, K, V, S>;
+	type Vac<'a, 'b> where Self: 'a = crate::KeyedRawVacantEntry<'a, 'b, Q, this_mod::RawVacantEntryMut<'a, K, V, S>>;
 
 	fn entry_ref<'a, 'b: 'a>(&'a mut self, key: &'b Q) -> Entry<Self::Occ<'a>, Self::Vac<'a, 'b>> {
 		let mut raw = self.raw_entry_mut();
 		match raw.from_key(key) {
-			hash_map::RawEntryMut::Occupied(occ) => {Entry::Occupied(occ)}
-			hash_map::RawEntryMut::Vacant(vac) => {
+			this_mod::RawEntryMut::Occupied(occ) => {Entry::Occupied(occ)}
+			this_mod::RawEntryMut::Vacant(vac) => {
 				Entry::Vacant(crate::KeyedRawVacantEntry{key, raw: vac})
 			}
 		}
